9f9df01fb7e96b949fc985d4063fab401aa1f6fd
Aathil Ahamed <2017is003@stu.ucsc.cmb.ac.lk>
2018-11-05 09:02:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title> JOB HUB </title>
+		 <link rel="stylesheet" href="">
+	</head>
+	
+	<body>
+		<div id="outline">
+
+			<!-- Header Section -->
+			<div id="header">
+				<img src="" id=""/>
+				<h1> JOB HUB </h1> <button id="login">
+
+				<form >
+   				<input type="submit" value="LOG IN" action="login.php"/>
+   				<input type="submit" value="SIGN UP AS A JOB PROVIDER" action="signupjp.php" />
+   				<input type="submit" value="SIGN UP AS A JOB SEEKER" action="signupjs.php"/>
+				</form>
+
+			</div>
+
+
+			<!-- Content Section -->
+			
+			
+			<div id="banner" style="background-image: url(images/back.jpg)" >
+				<!-- Announce -->
+					We’ve over <strong>15 0000</strong> Post offers for you!
+			</div>
+
+
+			<!-- Footer Section -->
+
+			<div id="footer">
+
+				<h4>Follow Us</h4>
+				<ul class="social-icons">
+					<li> <a href="#"> <img src="" id="fb"/>	</a>	</li>
+					<li> <a href="#"> <img src="" id="twitter"/>	</a>	</li>
+					<li> <a href="#"> <img src="" id="insta"/>	</a>	</li>
+					<li> <a href="#"> <img src="" id="linkdin"/>	</a>	</li>
+				</ul>
+				<div class="copyrights">©  Copyright 2018 by <a href="#">UCSC</a>. All Rights Reserved.</div>
+			</div>
+		
+		</div>
+	</body>
+	
+<html>
+		
